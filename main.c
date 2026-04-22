b3ba4ea5aef685c7f23816f4f9f35224153e1e6c
Soukie007 <Nmsoukmandjiev007@outlook.com>
2026-04-22 16:39:56 +0100

    Lava

--- main.c
+++ main.c
@@ -6,9 +6,9 @@
 #define GRID_H 200
 #define PIXEL_SCALE 4
 #define UI_HEIGHT 80
-#define TOTAL_BLOCKS 4
+#define TOTAL_BLOCKS 5
 
-typedef enum { AIR = 0, SAND, WATER, ROCK } blockType;
+typedef enum { AIR = 0, SAND, WATER, ROCK, LAVA } blockType;
 
 typedef struct {
     blockType type;
@@ -22,13 +22,13 @@ bool inBounds(int x, int y) {
     return x >= 0 && x < GRID_W && y >= 0 && y < GRID_H;
 }
 
-
 Color getBlockColor(block bloc) {
     float variation = (float)bloc.colorVariation / 255.0f; 
     switch (bloc.type) {
         case SAND:  return (Color){ (unsigned char)(230 + (variation * 25)), (unsigned char)(190 + (variation * 40)), (unsigned char)(100 + (variation * 50)), 255 };
         case WATER: return (Color){ (unsigned char)(30 + (variation * 20)), (unsigned char)(100 + (variation * 50)), (unsigned char)(200 + (variation * 55)), 255 };
         case ROCK:  return (Color){ (unsigned char)(80 + (variation * 40)), (unsigned char)(80 + (variation * 40)), (unsigned char)(85 + (variation * 40)), 255 };
+        case LAVA:  return (Color){ (unsigned char)(220 + (variation * 35)), (unsigned char)(60 + (variation * 80)), 0, 255 };
         default:    return BLACK;
     }
 }
@@ -38,26 +38,52 @@ void moveBlock(int x1, int y1, int x2, int y2) {
     grid[y1][x1] = grid[y2][x2];
     grid[y2][x2] = temp;
     grid[y2][x2].updated = true;
-    grid[y1][x1].updated = true;
+}
+
+
+void lavaWaterInteraction(int x, int y) {
+    int dx[] = {0, 0, -1, 1};
+    int dy[] = {-1, 1, 0, 0};
+    for (int i = 0; i < 4; i++) {
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+        if (inBounds(nx, ny) && grid[ny][nx].type == WATER) {
+            grid[y][x].type = ROCK;
+            grid[y][x].colorVariation = (unsigned char)(rand() % 256);
+            grid[ny][nx].type = ROCK;
+            grid[ny][nx].colorVariation = (unsigned char)(rand() % 256);
+            return;
+        }
+    }
 }
 
 void updateGrid(void) {
+    static int frameCount = 0;
+    frameCount++;
+    bool leftToRight = (frameCount % 2 == 0);
+    bool lavaCanMove = (frameCount % 3 == 0); 
+
     for (int y = 0; y < GRID_H; y++) {
         for (int x = 0; x < GRID_W; x++) grid[y][x].updated = false;
     }
 
     for (int y = GRID_H - 1; y >= 0; y--) {
-        for (int x = 0; x < GRID_W; x++) {
+        for (int i = 0; i < GRID_W; i++) {
+            int x = leftToRight ? i : (GRID_W - 1 - i);
             block *bloc = &grid[y][x];
             if (bloc->type == AIR || bloc->type == ROCK || bloc->updated) continue;
 
             if (bloc->type == SAND) {
                 if (inBounds(x, y + 1) && (grid[y+1][x].type == AIR || grid[y+1][x].type == WATER)) {
                     moveBlock(x, y, x, y + 1);
-                } else if (inBounds(x - 1, y + 1) && (grid[y+1][x-1].type == AIR || grid[y+1][x-1].type == WATER)) {
-                    moveBlock(x, y, x - 1, y + 1);
-                } else if (inBounds(x + 1, y + 1) && (grid[y+1][x+1].type == AIR || grid[y+1][x+1].type == WATER)) {
-                    moveBlock(x, y, x + 1, y + 1);
+                } else {
+                    int first = leftToRight ? -1 : 1;
+                    int second = leftToRight ? 1 : -1;
+                    if (inBounds(x + first, y + 1) && (grid[y+1][x+first].type == AIR || grid[y+1][x+first].type == WATER)) {
+                        moveBlock(x, y, x + first, y + 1);
+                    } else if (inBounds(x + second, y + 1) && (grid[y+1][x+second].type == AIR || grid[y+1][x+second].type == WATER)) {
+                        moveBlock(x, y, x + second, y + 1);
+                    }
                 }
             } 
             else if (bloc->type == WATER) {
@@ -76,10 +102,31 @@ void updateGrid(void) {
                     }
                 }
             }
+            else if (bloc->type == LAVA) {
+                lavaWaterInteraction(x, y);
+                if (grid[y][x].type != LAVA) continue; 
+
+                if (!lavaCanMove) continue;
+
+                if (inBounds(x, y + 1) && grid[y+1][x].type == AIR) {
+                    moveBlock(x, y, x, y + 1);
+                } else if (inBounds(x - 1, y + 1) && grid[y+1][x-1].type == AIR) {
+                    moveBlock(x, y, x - 1, y + 1);
+                } else if (inBounds(x + 1, y + 1) && grid[y+1][x+1].type == AIR) {
+                    moveBlock(x, y, x + 1, y + 1);
+                } else {
+                    int moveDir = leftToRight ? 1 : -1;
+                    if (inBounds(x + moveDir, y) && grid[y][x + moveDir].type == AIR) {
+                        moveBlock(x, y, x + moveDir, y);
+                    } else if (inBounds(x - moveDir, y) && grid[y][x - moveDir].type == AIR) {
+                        moveBlock(x, y, x - moveDir, y);
+                    }
+                }
+            }
         }
     }
 }
-
+#include <stdio.h>
 int main(void) {
     InitWindow(GRID_W * PIXEL_SCALE, (GRID_H * PIXEL_SCALE) + UI_HEIGHT, "sand falling type shit");
     SetTargetFPS(60);
@@ -93,6 +140,7 @@ int main(void) {
         if (IsKeyPressed(KEY_TWO)) selected = SAND;
         if (IsKeyPressed(KEY_THREE)) selected = WATER;
         if (IsKeyPressed(KEY_FOUR)) selected = ROCK;
+        if (IsKeyPressed(KEY_FIVE)) selected = LAVA;
 
         float wheel = GetMouseWheelMove();
         if (wheel != 0) {
@@ -100,7 +148,7 @@ int main(void) {
             if (brushRadius < 0) brushRadius = 0; 
             if (brushRadius > 20) brushRadius = 20; 
         }
-
+        printf("%d\n",GetFPS());
         Vector2 mousePos = GetMousePosition();
         int gridX = (int)mousePos.x / PIXEL_SCALE;
         int gridY = (int)mousePos.y / PIXEL_SCALE;
@@ -116,7 +164,6 @@ int main(void) {
                                 grid[spawnY][spawnX].type = AIR;
                             } else if (grid[spawnY][spawnX].type == AIR) {
                                 grid[spawnY][spawnX].type = selected;
-                                // Assign a random color variation when placed
                                 grid[spawnY][spawnX].colorVariation = (unsigned char)(rand() % 256);
                             }
                         }
@@ -146,8 +193,8 @@ int main(void) {
         int uiTop = GRID_H * PIXEL_SCALE;
         DrawRectangle(0, uiTop, GetScreenWidth(), UI_HEIGHT, DARKGRAY);
         
-        char* blockNames[] = {"ERASE", "SAND", "WATER", "ROCK"};
-        Color colors[] = {LIGHTGRAY, GOLD, BLUE, GRAY};
+        char* blockNames[] = {"ERASE", "SAND", "WATER", "ROCK", "LAVA"};
+        Color colors[] = {LIGHTGRAY, GOLD, BLUE, GRAY, ORANGE};
         
         for (int i = 0; i < TOTAL_BLOCKS; i++) {
             int xPos = i * (GetScreenWidth() / TOTAL_BLOCKS);
@@ -161,12 +208,14 @@ int main(void) {
             DrawText(blockNames[i], button.x + (button.width/2) - (textWidth/2), button.y + (button.height/2) - 10, fontSize, (selected == i) ? BLACK : WHITE);
         }
 
-        DrawText("Dont forget to use the mouse wheel to make the brush larger or smaller!",0,0,20,WHITE); //todo make this disappear when someone actualtly scrolls
+        DrawText("Dont forget to use the mouse wheel to make the brush larger or smaller!", 0, 0, 20, WHITE);
+
         if (mousePos.y < (GRID_H * PIXEL_SCALE)) {
             Color previewCol = YELLOW;
             if (selected == AIR) previewCol = WHITE;
             else if (selected == WATER) previewCol = BLUE;
-            else if (selected == ROCK) previewCol = GRAY;
+            else if (selected == ROCK)  previewCol = GRAY;
+            else if (selected == LAVA)  previewCol = ORANGE;
 
             DrawCircleLines(mousePos.x, mousePos.y, (brushRadius + 0.5f) * PIXEL_SCALE, WHITE);
             DrawCircle(mousePos.x, mousePos.y, (brushRadius + 0.5f) * PIXEL_SCALE, Fade(previewCol, 0.3f));
